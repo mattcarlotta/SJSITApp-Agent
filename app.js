9b8d7509c096e779021cfaed772e6ff977d951eb
Matt <carlotta.matt@gmail.com>
2019-10-20 20:24:44 -0700

    Move to babel-node

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-require("@babel/register");
-require("./service");
+import "middlewares";
+import { scheduleJob } from "node-schedule";
+import {
+  pollEmails, pollEvents, pollForms, pollSchedules,
+} from "libs";
+import { initiatedLogger } from "loggers";
+
+//= ===========================================================//
+// CREATE POLLING SERVICES                                     //
+//= ===========================================================//
+
+scheduleJob("*/30 * * * * *", async () => {
+  console.log(initiatedLogger());
+
+  await pollForms();
+  await pollEvents();
+  await pollEmails();
+});
+
+// scheduleJob("*/5 * * * * *", async () => {
+//   await pollSchedules();
+// });
+
+scheduleJob("59 7 25 * *", async () => {
+  await pollSchedules();
+});
